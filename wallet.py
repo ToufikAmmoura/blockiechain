51644156211b1aa166b3fade252be675caef989d
ToufikAmmoura <toufikammoura@gmail.com>
2021-12-29 14:48:00 +0100

    stappen aan het maken in wallet

--- wallet.py
+++ wallet.py
@@ -25,6 +25,43 @@ def init_wallet(name):
         f.write(privkey.export_key(format='DER'))
         f.close()
 
+def get_key_from_wallet(name):
+    filename = name + '.der'
+    try:
+        f = open(filename, 'rt')
+        key = ECC.import_key(f.read())
+        return key
+    except FileNotFoundError:
+        print('Wallet does not exist')
+        return None
+
+def get_balance(address, unspent_txouts):
+    amount = 0
+    for utxo in unspent_txouts:
+        if utxo.address == address:
+            amount += utxo.amount
+    return amount
+
+def find_unspent_txouts(address, unspent_txouts):
+    utxos = []
+    for utxo in unspent_txouts:
+        if utxo.address == address:
+            utxos.append(utxo)
+    return utxos
+
+def find_txouts_for_amount(amount, unspent_txouts):
+    current_amount = 0
+    included_utxos = []
+    for utxo in unspent_txouts:
+        current_amount += utxo.amount
+        included_utxos.append(utxo)
+        if current_amount >= amount:
+            leftover = current_amount - amount
+            return (included_utxos, leftover)
+
+    print("Cannot create transaction from the available unpent transaction outputs")
+    return None # hier misschien beter over nadenken
+
 def delete_wallet(name):
     filename = name + '.der'
     if os.path.exists(filename):
